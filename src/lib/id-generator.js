7e6018600862cde4b70e674f1398781752b493ab
Dean Netherton <dnetherton@dius.com.au>
2019-07-14 17:00:30 +1000

    Fix issue with socket identity with slashes, causing incorrect mqtt topics to be created

--- src/lib/id-generator.js
+++ src/lib/id-generator.js
@@ -1,7 +1,8 @@
 import uuidv4 from 'uuid/v4'
+import bs58 from 'bs58'
 
 export function generateId() {
   const guid = uuidv4().replace(/-/g, '')
-  const r = Buffer.from(guid, 'hex').toString('base64')
+  const r = bs58.encode(Buffer.from(guid, 'hex'))
   return r.slice(0, r.length - 2)
 }
